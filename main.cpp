a833ff94eeb1833dd862b9a44837ca8b5846d2a2
zouzhichao <zouzhichao@meituan.com>
2019-08-24 20:09:08 +0800

    jd

--- main.cpp
+++ main.cpp
@@ -8,7 +8,33 @@
 using namespace std;
 
 int main() {
+    int len,i;
+    cin >> len;
+    string str;
 
+    cin >> str;
+    int** dp;
+    dp = new int*[len + 1];
+    for (int i = 0; i <= len; ++i) {
+        dp[i] = new int[2];
+    }
+    dp[0][0] = 0;
+    dp[0][1] = 0x3f3f3f;
+    for(i = 0; i < len;i++)
+    {
+        if(str[i]>='a' && str[i]<='b')
+        {
+            dp[i+1][0]=min(dp[i][0]+1,dp[i][1]+2);
+            dp[i+1][1]=min(dp[i][0]+2,dp[i][1]+2);
+        }
+        else
+        {
+            dp[i+1][0]=min(dp[i][0]+2,dp[i][1]+2);
+            dp[i+1][1]=min(dp[i][0]+2,dp[i][1]+1);
+        }
+    }
+    cout << min(dp[len][0],dp[len][1]+1) << endl;
     return 0;
+
 
 }
