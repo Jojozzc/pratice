58da8b30fa1b1c3574529649b5dc1ea1305584f3
zouzhichao <zouzhichao@meituan.com>
2019-08-24 20:32:10 +0800

    jd

--- main.cpp
+++ main.cpp
@@ -12,27 +12,29 @@ int main() {
     cin >> len;
     string str;
     cin >> str;
-    int** dp;
-    dp = new int*[len + 1];
-    for (int i = 0; i <= len; ++i) {
-        dp[i] = new int[2];
+    int dp[1000000][2];
+//    dp[0][0] = 0;
+//    dp[0][1] = 1;
+    if(str[0]<='Z'){
+        dp[0][0]=2;
+        dp[0][1]=3;
     }
-    dp[0][0] = 0;
-    dp[0][1] = 1;
+    else {
+        dp[0][0]=1;
+        dp[0][1]=4;
+    }
     for(int i = 0; i < len;i++)
     {
-        if(str[i]>='A'&&str[i]<='Z')
-        {
-            dp[i+1][1]=min(dp[i][0]+2,dp[i][1]);
-            dp[i+1][0]=min(dp[i][0]+1,dp[i][1]+1);
+        if(str[i]<='Z'){
+            dp[i][0] = min(dp[i-1][0]+2,dp[i-1][1]+2);
+            dp[i][1] = min(dp[i-1][0]+3,dp[i-1][1]+1);
         }
-        else
-        {
-            dp[i+1][1]=min(dp[i][0]+1,dp[i][1]+1);
-            dp[i+1][0]=min(dp[i][0],dp[i][1]+2);
+        else{
+            dp[i][0]=min(dp[i-1][0]+1,dp[i-1][1]+1);
+            dp[i][1]=min(dp[i-1][0]+4,dp[i-1][1]+2);
         }
     }
-    cout << min(dp[len][0],dp[len][1]+1) << endl;
+    cout << min(dp[len - 1][0],dp[len - 1][1]+1) << endl;
     return 0;
 
 
