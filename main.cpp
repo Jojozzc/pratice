9d8e2ca23ebf9451ebdd514da5ceeda8a2b811f9
zouzhichao <zouzhichao@meituan.com>
2019-08-29 20:22:37 +0800

    sf1

--- main.cpp
+++ main.cpp
@@ -1,47 +1,40 @@
 #include<iostream>
-#include <string>
-#include <stack>
-#include <map>
 #include <vector>
-#include <string>
 #include <algorithm>
 using namespace std;
 
+
+int lengthOfLongest(vector<int>& nums) {
+    if (nums.size() == 0) {
+        return 0;
+    }
+    int len = nums.size();
+    int* dp = new int[nums.size()];
+    dp[0] = 1;
+    int maxLen = 1;
+    for (int i = 1; i < len; i++) {
+        int maxVal = 0;
+        for (int j = 0; j < i; j++) {
+            if (nums[i] > nums[j]) {
+                maxVal = max(maxVal, dp[j]);
+            }
+        }
+        dp[i] = maxVal + 1;
+        maxLen = max(maxLen, dp[i]);
+    }
+    return maxLen;
+}
+
 int main() {
-    int n;
+    int n = 0;
     cin >> n;
-    int* nums = new int[n];
-    for(int i = 0; i < n; ++i){
-        cin >> nums[i];
+    vector<int> nums;
+    for (int i = 0; i < n; ++i) {
+        int temp;
+        cin >> temp;
+        nums.push_back(temp);
     }
-    stack<int*> st;
-    int init[2] = {0, 0};
-    st.push(init);
-    int lastMax = 0;
-
-    int maxNum = nums[0];
-    int minNum = nums[0];
-
-    for(int i = 1; i < n; ++i){
-        while (nums[i] < lastMax){
-            int* tmp = st.top();
-            st.pop();
-            maxNum = max(maxNum, tmp[1]);
-            minNum = max(minNum, tmp[0]);
-            minNum = min(nums[i], minNum);
-            lastMax = st.top()[1];
-        }
-        minNum = min(nums[i], minNum);
-        if(nums[i] >= maxNum){
-            int tArr[] = {minNum, maxNum};
-            st.push(tArr);
-            lastMax = maxNum;
-            maxNum = nums[i];
-            minNum = nums[i];
-        }
-    }
-    cout << st.size() << endl;
+    cout << lengthOfLongest(nums);
     return 0;
-
 
 }
