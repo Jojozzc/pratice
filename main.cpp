c5ee80721446d6a3d40ed8da1bb95ca335f4ddef
zouzhichao <zouzhichao@meituan.com>
2019-09-07 19:16:58 +0800

    hw1

--- main.cpp
+++ main.cpp
@@ -5,63 +5,28 @@ using namespace std;
 
 
 int main() {
+    vector<int> nums;
     int n = 0;
-    int result = 0;
-    cin >> n;
-    if (n <= 1) {
-        cout << 1;
-        return 0;
+    while (cin >> n) {
+        nums.push_back(n);
     }
-    int* nums = new int[n];
-
-    for (int i = 0; i < n; ++i) {
-        cin >> nums[i];
+    int len = nums.size();
+    int* dp = new int[len];
+    for (int i = 0; i < len / 2; ++i) {
+        dp[i] = 1;
     }
-
-
-    int start = 0;
-
-    int single = 1;
-    while (start < n) {
-        while (start < n && nums[start] == 1) {
-            start++;
-        }
-        if (start == n) {
-            break;
-        }
-        int p = start;
-        while (p < n && nums[p] == 0) {
-            p++;
-        }
-        // p是1或者边界的位置
-        int zeroCount = p - start;
-        if (start > 0 && p < n) {
-            // 前后都是1
-            if (zeroCount <= 2) {
-                single = 0;
-            } else if (zeroCount % 2 == 1) {
-                // 奇数个数的0只能有一种改法
-                single = zeroCount / 2;
-            } else {
-                // 个数是偶数
-                single = (zeroCount - 1) / 2;
+    for (int i = len / 2; i < len; ++i) {
+        dp[i] = 999999;
+        for (int j = 0; j < i; ++j) {
+            if (nums[j] + j == i) {
+                dp[i] = min(dp[j] + 1, dp[i]);
             }
-        } else if (start == 0 && p == n){
-            // 前后都没东西
-            if (zeroCount % 1 == 1) {
-                single = (zeroCount + 1) / 2;
-            } else {
-                single = zeroCount / 2;
-            }
-        } else {
-            // 前后只有一个地方是1
-            single = zeroCount / 2;
         }
-
-        start = p;
-        result += single;
     }
-    cout << result << endl;
+    if (dp[len - 1] >= len) {
+        cout << -1 << endl;
+    } else {
+        cout << dp[len - 1];
+    }
     return 0;
-
 }
