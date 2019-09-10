08e82ca8bf45651f069869b2d567a2ceef33c4b0
zouzhichao <zouzhichao@meituan.com>
2019-09-10 21:46:21 +0800

    dx2

--- main.cpp
+++ main.cpp
@@ -1,31 +1,22 @@
-import java.util.Scanner;
+#include <string>
+#include <iostream>
 
-public class Main {
-public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        String arrStr = in.nextLine();
-        arrStr = arrStr.substring(1, arrStr.length() - 1);
-        String[] numsStr = arrStr.split(",");
-        if (numsStr.length == 0) {
-            System.out.println(0);
-            return;
-        }
-        int[] nums = new int[numsStr.length];
-        int res = nums[0];
-        int curSum = 0;
-        for (int i = 0; i < nums.length; i++) {
-            numsStr[i] = numsStr[i].trim();
-            nums[i] = Integer.parseInt(numsStr[i]);
-        }
-        for (int i = 0; i < nums.length; i++) {
-            if (curSum > 0) {
-                curSum += nums[i];
-            } else {
-                curSum = nums[i];
-            }
-            res = Math.max(res, curSum);
-        }
-        System.out.println(res);
+using namespace std;
+string intToRoman(int num) {
+    string ones[] = { "","I", "II", "III","IV","V","VI","VII","VIII","IX" };
+    string tens[] = { "", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC" };
+    string hunds[] = { "", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM" };
+    string thous[] = { "", "M", "MM", "MMM" };
 
-    }
+    string ans = "";
+    ans += thous[num / 1000];
+    ans += hunds[num / 100 % 10];
+    ans += tens[num / 10 % 10];
+    ans += ones[num % 10];
+    return ans;
+}
+
+int main(){
+    cout << intToRoman(15);
+    return 0;
 }
