0a612c53dd65d45724dca2a45a770e96bcee4e81
zouzhichao <zouzhichao@meituan.com>
2019-09-12 21:35:44 +0800

    58-2

--- main.cpp
+++ main.cpp
@@ -6,23 +6,44 @@ public class Main {
 public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         String str = in.next();
-        count(str);
+        statisticSubStr(str);
 
 
 
     }
-private static void count(String ss){
-
-        String[] words = ss.split(",");
-        Map<String, Integer> countMap = new HashMap<>();
-        for (String str : words) {
-            if (str.isEmpty()) {
-                continue;
-            }
-            char ch = str.charAt(str.length() - 1);
-            if (ch == 'd' || ch == 'e')
-                countMap.put(str, countMap.getOrDefault(str, 0) + 1);
+private static void statisticSubStr(String s){
+        if (s == null || s.isEmpty()) {
+            return;
         }
-        System.out.println(countMap);
+        int left = 0;
+        int[] count = new int[26];
+        PriorityQueue<int[]> heap = new PriorityQueue<>(new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o2[1] - o1[1];
+            }
+        });
+        while (left < s.length()) {
+            int right = left;
+            while (right < s.length() && (s.charAt(right) == s.charAt(left))) {
+                right++;
+            }
+            if (right - left > 1) {
+                count[s.charAt(left) - 'a'] += (right - left);
+            }
+            left = right;
+        }
+        for (int i = 0; i < 26; i++) {
+            if (count[i] > 0) {
+                heap.add(new int[]{i, count[i]});
+            }
+        }
+        while (!heap.isEmpty()) {
+            int[] pair = heap.poll();
+            StringBuilder str = new StringBuilder();
+            str.append((char)(pair[0] + 'a')).append(":").append(pair[1]);
+            System.out.println(str.toString());
+        }
+
     }
 }
