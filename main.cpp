5cd27676d37abb3702745066b09dbee05da5b2c0
zouzhichao <zouzhichao@meituan.com>
2019-09-16 15:41:14 +0800

    gz1

--- main.cpp
+++ main.cpp
@@ -2,40 +2,32 @@
 #include <vector>
 using namespace std;
 
+
+int numDecodings(string s) {
+    int cur = 1;
+    int pre = 1;
+    for(int i = 1; i < s.length(); i++){
+        int num = (s[i - 1] - '0') * 10 + (s[i] - '0');
+        int temp = cur;
+        if(s[i] == '0'){
+            if(num > 26 || num == 0) return 0;
+            cur = pre;
+        }
+        else{
+            if(s[i - 1] == '0') cur = pre;
+            else {
+                if(num <= 26) cur += pre;
+            }
+        }
+
+        pre = temp;
+    }
+    return cur;
+}
+
 int main(){
-    int n = 0;
-    cin >> n;
-    int cityNum = n + 1;
-    int* a = new int[n + 2];
-    int* b = new int[n + 2];
-    long total = 0L;
-    int* left = new int[n + 2]; // 第i个城市还差几个怪没杀
-
-
-    for (int i = 1; i <= cityNum; ++i) {
-        cin >> a[i];
-    }
-    for (int i = 1; i <= n; ++i) {
-        cin >> b[i];
-    }
-    left[0] = 0;
-    left[1] = a[1];
-    for (int i = 1; i <= n; ++i) {
-        int killNum = b[i];
-        if (left[i] >= killNum) {
-            left[i] -= killNum;
-            killNum = 0;
-        } else {
-            killNum = killNum - left[i];
-            left[i] = 0;
-        }
-        if (killNum > 0) {
-            left[i + 1] = max(0, a[i + 1] - killNum);
-            killNum = max(0, killNum - a[i + 1]);
-        }
-        total += (b[i] - killNum);
-    }
-    total += (a[n + 1] - left[n + 1]);
-    cout << total << endl;
+    string s;
+    cin >> s;
+    cout << numDecodings(s) << endl;
     return 0;
 }
