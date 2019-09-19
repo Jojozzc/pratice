51b3491afb66557c4b283092ce0fa243e0a648b1
zouzhichao <zouzhichao@meituan.com>
2019-09-19 20:17:04 +0800

    紫光1 排列组合数目

--- main.cpp
+++ main.cpp
@@ -2,26 +2,13 @@
 #include <vector>
 
 using namespace std;
+
+int fact[] = {1,1,2,6,24,120,720};
+
 int main(){
-    vector<int> nums;
     int n = 0;
-    while (cin >> n) {
-        nums.push_back(n);
-    }
-    int k = 0;
-    for (int i = 1; i < nums.size(); ++i) {
-        if (nums[i] == nums[k]) {
-            continue;
-        } else{
-            k++;
-            nums[k] = nums[i];
-        }
-    }
-    for (int i = 0; i <= k; ++i) {
-        if (i > 0) {
-            cout << " ";
-        }
-        cout << nums[i];
-    }
+    cin >> n;
+    cout << fact[6] / fact[6 - n] << endl;
     return 0;
 }
+
